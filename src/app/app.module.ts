e54c5e8140c10bdfee468a6d88a3588c5bdebb60
Bryson69 <brysonmundia@gmail.com>
2020-06-15 13:23:05 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [ AppComponent, QuoteComponent, QuoteDetailComponent, HighlightDirective, DateCountPipe, QuoteFormComponent ],
+  imports: [ BrowserModule, AppRoutingModule,  FormsModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
